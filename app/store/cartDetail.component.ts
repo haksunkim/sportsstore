20c1fd1e392d0a5c526e38b4180bcf77cbeba5f2
Haksun Kim <itpro@haksunkim.com>
2017-02-13 14:34:27 +1000

    added CartDetailComponent and CheckoutComponent to feature module
    - added CartDetailComponent and CheckoutComponent to store.module

--- app/store/cartDetail.component.ts
+++ app/store/cartDetail.component.ts
@@ -3,4 +3,4 @@ import { Component } from "@angular/core";
 @Component({
   template: `<div><h3 class="bg-info p-a-1">Cart Detail Component</h3></div>`
 })
-export class CardDetailComponent {}
+export class CartDetailComponent {}
